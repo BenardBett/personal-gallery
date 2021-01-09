88af7b5ba5af9b8bd735496782e3ae9fa567eb26
BenardBett <kiplangatbett5@gmail.com>
2021-01-09 12:14:17 +0300

    define the paths for methods in view.py file

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', home, name="home")
+    path('about/', about, name="about"),
+    path('images/',images),
+    path('search_category/', search_category, name="search_category"),
 ]
